1490563cfe8df08c80fb57cd87b00db903b9bfda
cmckinney0501 <161032783+cmckinney0501@users.noreply.github.com>
2024-02-23 09:34:22 -0500

    Week 6
    Assignment

---
+++ Week6Assignment/Pages/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Week6Assignment.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public string? Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = $"Hello World {DateTime.Now.ToLongTimeString()}";
+        }
+    }
+}
